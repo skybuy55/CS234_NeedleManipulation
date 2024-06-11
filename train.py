36025b12bf57f6c35a859bc16eb23f7b1b799c1d
JiayingFang <99144219+JiayingFang@users.noreply.github.com>
2024-06-11 16:08:57 -0700

    Update train.py

--- train.py
+++ train.py
@@ -11,14 +11,14 @@ set_random_seed(100, using_cuda=True)
 
 env = gym.make("NeedleReach-v0")
 
-eval_callback = EvalCallback(env, n_eval_episodes=20, best_model_save_path='./logs/SAC_image_seed100', 
+eval_callback = EvalCallback(env, n_eval_episodes=20, best_model_save_path='./logs/SAC_seed100', 
                             log_path='./logs/', eval_freq=2500, deterministic=True)
 
-checkpoint_callback = CheckpointCallback(save_freq=8000, save_path='./logs/SAC_image_seed100', name_prefix='rl_model',
+checkpoint_callback = CheckpointCallback(save_freq=8000, save_path='./logs/SAC_seed100', name_prefix='rl_model',
                                          save_replay_buffer=False, save_vecnormalize=False)
 
 callback = CallbackList([eval_callback, checkpoint_callback])
 
-model = SAC("MultiInputPolicy", env,buffer_size=20000, learning_starts=2000, verbose=1, tensorboard_log='./tensorboard/SAC_image_seed100')
+model = SAC("MultiInputPolicy", env,buffer_size=20000, learning_starts=2000, verbose=1, tensorboard_log='./tensorboard/SAC_seed100')
 print(model.policy)
 model.learn(total_timesteps=80000, callback=callback)
